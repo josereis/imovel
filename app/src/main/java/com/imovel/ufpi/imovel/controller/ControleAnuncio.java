c5c9529bca40f9b84dfd449ac1816a4bf47d4540
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2017-07-08 12:15:22 -0300

    Alteração da classe controladora de anuncios de modo a atualizar a lista de anuncios local(apenas foi montado um anuncio para ser adicionado a lista de anuncios), para os testes iniciais.

--- app/src/main/java/com/imovel/ufpi/imovel/controller/ControleAnuncio.java
+++ app/src/main/java/com/imovel/ufpi/imovel/controller/ControleAnuncio.java
@@ -2,7 +2,9 @@ package com.imovel.ufpi.imovel.controller;
 
 import com.imovel.ufpi.imovel.models.Anuncio;
 import com.imovel.ufpi.imovel.models.AnuncioAluguel;
+import com.imovel.ufpi.imovel.models.Endereco;
 import com.imovel.ufpi.imovel.models.Imovel;
+import com.imovel.ufpi.imovel.models.Usuario;
 
 import java.util.ArrayList;
 
@@ -14,42 +16,37 @@ public class ControleAnuncio {
     private ArrayList<Anuncio> anuncios = new ArrayList<Anuncio>();
 
     public ControleAnuncio() {
-        // imovel 1
-        Imovel imovel = new Imovel();
+        // criando endereço
+        Endereco end = new Endereco();
+        end.setEstado("PI");
+        end.setNumero("1004");
+        end.setCEP(64410000);
+        end.setBairro("Areias");
+        end.setCidade("Angical do Piauí");
+        end.setRua("Rua Marcelino Sousa");
 
+        // criando um usuario teste
+        Usuario usuario = new Usuario();
+        usuario.setEndereco(end);
+        usuario.setName("José Reis Ribeiro Santiago");
+        usuario.setEmail("jsribeirosantiago4@gmail.com");
+
+        // criando um imovel 1
+        Imovel imovel = new Imovel();
         imovel.setNumeroQuartos(3);
         imovel.setNumeroBanheiros(2);
-        imovel.setDescricao("imovel 1");
-        imovel.getEndereco().setEstado("PI");
-        imovel.getEndereco().setCidade("Teresina");
+        imovel.setEndereco(end);
+        imovel.setDescricao("Imovel em otima condição, rescem reformado, bem arejado e em otima localização.");
 
         // anuncio 1
         Anuncio anuncio = new AnuncioAluguel();
-
         anuncio.setImovel(imovel);
-        anuncio.setDescricao("Primeiro Anuncio Teste");
+        anuncio.setAnunciante(usuario);
 
         anuncios.add(anuncio);
     }
 
     public ArrayList<Anuncio> listar(){
         return anuncios;
-    }
-
-    public ArrayList<Anuncio> buscar(int tipo, int quartos, int banheiros, String cidade, String estado) {
-        ArrayList<Anuncio> result = new ArrayList<Anuncio>();
-
-        if(tipo == 1) { // criar constante 1 para alugel
-            for(Anuncio anuncio: anuncios) {
-                if(anuncio instanceof AnuncioAluguel) {
-                    if(anuncio.getImovel().getEndereco().getCidade().equals(cidade) && anuncio.getImovel().getEndereco().getEstado().equals(estado)
-                            && quartos == anuncio.getImovel().getNumeroQuartos() && banheiros == anuncio.getImovel().getNumeroBanheiros()) {
-                        result.add(anuncio);
-                    }
-                }
-            }
-        }
-
-        return result;
     }
 }
