1e6c7ea8b372c583696eba99e9bee03e2ccac183
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2017-07-08 12:24:52 -0300

    Atualização da Classe. Foi alterado a forma de listagem dos objetos da lista. Instancia o controlador de anuncio e chama apenas a lista de todos os anuncios cadastrados para a exibição.

--- app/src/main/java/com/imovel/ufpi/imovel/views/ListaAnunciosActivity.java
+++ app/src/main/java/com/imovel/ufpi/imovel/views/ListaAnunciosActivity.java
@@ -1,7 +1,11 @@
 package com.imovel.ufpi.imovel.views;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -10,50 +14,69 @@ import com.imovel.ufpi.imovel.R;
 import com.imovel.ufpi.imovel.controller.ControleAnuncio;
 import com.imovel.ufpi.imovel.models.Anuncio;
 import com.imovel.ufpi.imovel.models.Usuario;
+import com.imovel.ufpi.imovel.utils.ClickRecyclerView_Interface;
+import com.imovel.ufpi.imovel.utils.RecyclerAdapter;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListaAnunciosActivity extends AppCompatActivity implements Serializable {
-    private Usuario usuario = new Usuario();
+public class ListaAnunciosActivity extends AppCompatActivity implements ClickRecyclerView_Interface{
+    private RecyclerAdapter adapter;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton floatingActionButton;
+    private List<Anuncio> anuncios = new ArrayList<Anuncio>();
+
+    public void setMRecyclerView() {
+        // Aqui é instanciado o Recyclerview
+        this.mRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewAnuncios);
+
+        this.mLayoutManager = new LinearLayoutManager(this);
+        this.mRecyclerView.setLayoutManager(mLayoutManager);
+
+        this.adapter = new RecyclerAdapter(this, anuncios, this);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    public void buscaAnuncios() {
+        anuncios = (new ControleAnuncio()).listar();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_anuncios);
 
-
-        //pega dados do usuario corrente da aplicacao
-        Bundle dadosUsuario = getIntent().getExtras();
-        if (dadosUsuario != null){
-            usuario.setName(dadosUsuario.getString("nomeUsuario"));
-            usuario.setEmail(dadosUsuario.getString("emailUsuario"));
-        }
-        ListView listaAnuncios = (ListView) findViewById(R.id.listViewAnuncios);
-
-        this.listarAnuncios(listaAnuncios);
-    }
-
-    public void listarAnuncios(ListView listaAnuncios) {
-        ControleAnuncio controleAnuncio = new ControleAnuncio();
-        ArrayList<Anuncio> anuncios = new ArrayList<Anuncio>();
-
-        anuncios = controleAnuncio.listar();
+        anuncios = (new ControleAnuncio()).listar();
         if(anuncios.size() > 0) {
-            ArrayAdapter<Anuncio> adapter = new ArrayAdapter<Anuncio>(this, android.R.layout.simple_list_item_1, anuncios);
 
-            listaAnuncios.setAdapter(adapter);
+            // tratamento dos casos quando possui uma quantidade de anuncios a ser exibida
+
+        } else {
+            String mensagem="Não foi retornado o anuncio!";
+            Toast toast = Toast.makeText(this, mensagem, Toast.LENGTH_SHORT);
+            toast.show();
         }
 
+        setMRecyclerView();
     }
 
     @Override
     public void onBackPressed() {
-
         String mensagem="Voltar para tela anterior.";
         Toast toast = Toast.makeText(this, mensagem, Toast.LENGTH_SHORT);
         toast.show();
     }
+
+    /**
+     * Método onde é tratado o click em um item da lista
+     * @param object
+     */
+    @Override
+    public void onCustomClick(Object object) {
+
+    }
+
 
 }
